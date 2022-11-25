7ff3bba7ddc0c969ce445a5159916a5442ca1027
DESKTOP-TM8FG9P\Ali Ahmed <alii.amadd@gmail.com>
2022-11-25 13:09:09 +0500

    static env

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,8 +4,10 @@ export const authOptions = {
 
   providers: [
     GoogleProvider ({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      // clientId: process.env.GOOGLE_CLIENT_ID,
+      // clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId:"153398833382-ceq3ih8jqf1p1nmu89ongahc6e292b95.apps.googleusercontent.com",
+      clientSecret:"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",
     }),
   ],
   
